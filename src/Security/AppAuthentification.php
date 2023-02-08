cbd9ca31fd40e7e8a1da028022797e15c6d23b11
Njorgo <rousseau.dev.pro@gmail.com>
2023-02-08 10:28:25 +0100

    Remenber me + logout dans la barre de nav

--- src/Security/AppAuthentification.php
+++ src/Security/AppAuthentification.php
@@ -11,6 +11,7 @@ use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
@@ -36,6 +37,7 @@ class AppAuthentification extends AbstractLoginFormAuthenticator
             new PasswordCredentials($request->request->get('motPasse', '')),
             [
                 new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
+                new RememberMeBadge(),
             ]
         );
     }
