096ab34513607b45ec1f5d1654b8ce08c1c79d94
beren <beren-07@hotmail.fr>
2023-02-13 17:50:15 +0100

    Mise en place des fonctions Inscriptions et Désistement : actions à paramétrer et design en cours de révision, sera fait prochainement.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
 use App\Entity\Sortie;
 use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
@@ -9,6 +10,7 @@ use DateInterval;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController {
@@ -30,7 +32,6 @@ class MainController extends AbstractController {
             6 =>'Annuler'
         ];
 
-
         /* Affichage des informations demandés pour les différentes sorties proposées */
         $listeInfosSortie = $sortieRepository->listeInfosSorties();
         $listeInfosSortieRender = [];
@@ -40,22 +41,21 @@ class MainController extends AbstractController {
         foreach($listeInfosSortie as $infosSortie) {
             $dureeInterval = DateInterval::createFromDateString(strval($infosSortie['duree']). ' min');
             $dateHeureFin = clone $infosSortie['dateHeureDebut'];
-            dump($infosSortie['dateHeureDebut']);
-            dump($dateHeureActuelle);
-            dump($dateHeureFin);
             date_add($dateHeureFin, $dureeInterval);
             if ($dateHeureActuelle >= $infosSortie['dateHeureDebut'] AND $dateHeureActuelle < $dateHeureFin) {
                 $infosSortie['libelle'] = 'Activité en cours';
                 $infosSortie['action'] = 'Afficher';
+                $infosSortie['action2'] = '';
             } elseif ($dateHeureActuelle > $infosSortie['dateHeureDebut']) {
                 $infosSortie['libelle'] = 'Passée';
                 $infosSortie['action'] = 'Afficher';
+                $infosSortie['action2'] = '';
             } else {
                 $infosSortie['libelle'] = 'Ouverte';
                 $infosSortie['action'] = 'Afficher';
             }
             if($infosSortie['organisateurId'] == $participant->getId() AND $dateHeureActuelle < $infosSortie['dateHeureDebut']){
-                $infosSortie['action'].= ' - Annuler';
+                $infosSortie['action2'] = ' - Annuler';
             }
             /* if($infosSortie['organisateurId'] == $participant->getId() AND $infosSortie['organisateurId'] == $infosSortie['sortie_ID']){
                  $infosSortie['action'].= ' - Se désister';
@@ -64,16 +64,57 @@ class MainController extends AbstractController {
                   $infosSortie['inscrit'] = 'X';
                   $infosSortie['action'] .= ' - Se désister';
               }*/
-            elseif($infosSortie['organisateurId'] != $participant->getId() AND $dateHeureActuelle < $infosSortie['dateHeureDebut']){
-                $infosSortie['action'].= " - S'inscrire";
+            elseif($dateHeureActuelle < $infosSortie['dateHeureDebut']){
+                $infosSortie['action2'] = " - S'inscrire";
             }
             array_push($listeInfosSortieRender, $infosSortie);
         }
 
         return $this->render('main/home.html.twig', [
-
             'listeInfosSortie' => $listeInfosSortieRender,
-
         ]);
+    }
+    /**
+     * @param Sortie $sortie_id
+     * @param Participant $participant_id
+     *
+     * @Route("/inscrire-sortie/{sortie_id}/{participant_id}", requirements={"'sortie_id" = "\d+", "participant_id" = "\d+" }, name="inscrire_sortie")
+     * @return RedirectResponse
+     *
+     */
+   public function inscrireSortie(
+       Sortie $sortie_id,
+       Participant $participant_id,
+       EntityManagerInterface $entityManagerInterface,
+    )
+   {
+
+       $sortieInscription = $sortie_id->addInscrit($participant_id);
+       $entityManagerInterface->persist($sortieInscription);
+       $entityManagerInterface->flush();
+
+       return $this->redirectToRoute('main_home');
+
+    }
+
+    /**
+     * @param Sortie $sortie_id
+     * @param Participant $participant_id
+     *
+     * @Route("/desistement-sortie/{sortie_id}/{participant_id}", requirements={"'sortie_id" = "\d+", "participant_id" = "\d+" }, name="desistement_sortie")
+     * @return RedirectResponse
+     *
+     */
+    public function desistementSortie(
+        Sortie $sortie_id,
+        Participant $participant_id,
+        EntityManagerInterface $entityManagerInterface)
+    {
+
+        $sortie_id->removeInscrit($participant_id);
+        $entityManagerInterface->flush();
+
+        return $this->redirectToRoute('main_home');
+
     }
 }
